c4e88d442004a56ef73f4f41169d2ea9a12cf07e
cxn <andrew@avisiblenetwork.com>
2025-09-18 06:34:40 -0700

    completed game loop

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-import pygame
+import pygame, constants
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        pygame.Surface.fill(screen,"black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
