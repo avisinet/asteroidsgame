61c3f43f5f4789bfc5d6407db217e4730eb107da
cxn <andrew@avisiblenetwork.com>
2025-09-19 07:37:53 -0700

    completed fps

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def main():
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     
     while True:
@@ -16,6 +19,7 @@ def main():
         pygame.Surface.fill(screen,"black")
         pygame.display.flip()
 
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
